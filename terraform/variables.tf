dec1163cff3ab5b04966cc436deba68952b50762
Blahodir <deweb4you@gmail.com>
2026-04-19 13:48:48 +0000

    fix: remove terraform binaries and add gitignore

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "fjord-step"
+}
